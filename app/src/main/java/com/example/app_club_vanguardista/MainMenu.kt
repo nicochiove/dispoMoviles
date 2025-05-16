caedcf1a0d064748ebdad333bfc0891682a1b375
Nicolás Chiovetta <nicochiove@gmail.com>
2025-05-16 19:26:55 -0300

    Update MainMenu.kt

--- app/src/main/java/com/example/app_club_vanguardista/MainMenu.kt
+++ app/src/main/java/com/example/app_club_vanguardista/MainMenu.kt
@@ -18,12 +18,31 @@ class MainMenu : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val btnOut = findViewById<Button>(R.id.btnOut)
+        btnOut.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+        val btnPagos = findViewById<Button>(R.id.btnPagos)
+        btnPagos.setOnClickListener {
+            val intent = Intent(this, BuscarSocio::class.java)
+            startActivity(intent)
+        }
+        val btnListas = findViewById<Button>(R.id.btnVenc)
+        btnListas.setOnClickListener {
+            val intent = Intent(this, ListasMenu::class.java)
+            startActivity(intent)
+        }
         val button = findViewById<Button>(R.id.btnAlta)
         button.setOnClickListener {
             val intent = Intent(this, Alta3::class.java)
             startActivity(intent)
         }
-
     }
 }
