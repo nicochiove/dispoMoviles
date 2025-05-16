15cfa24c378bd81c304c77535bba6bfa88578e42
nicochiove <nicochiove@gmail.com>
2025-05-16 14:37:40 -0300

    pantalla pao exitoso

---
+++ app/src/main/java/com/example/app_club_vanguardista/PagoExitoso.kt
@@ -0,0 +1,28 @@
+package com.example.app_club_vanguardista
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class PagoExitoso : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_pago_exitoso)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val btnBack = findViewById<Button>(R.id.btnOut)
+        btnBack.setOnClickListener {
+            val intent = Intent(this, BuscarSocio::class.java)
+            startActivity(intent)
+        }
+    }
+}
